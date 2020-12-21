6d0c3f3fed45df1a0b675c0e435db7fdb937addd
Michael Lodge-Paolini <michael.lodge-paolini@wexinc.com>
2020-12-21 17:05:00 -0500

    workflow test

--- src/automerge.js
+++ src/automerge.js
@@ -1,27 +1,94 @@
 const core = require('@actions/core');
 const GitHub = require('@actions/github');
+const semver = require('semver');
+
+const MERGE_ACTIONS = {
+  NONE: 'none',
+  MERGE: 'merge',
+  REQUEST: 'request'
+};
+
+/**
+ *
+ * @param config
+ * @returns {Promise<array>}
+ */
+async function getBranchHierarchy(config) {
+  // Get owner and repo from context of payload that triggered the action
+  const { owner: currentOwner, repo: currentRepo } = GitHub.context.repo;
+
+  // Get authenticated GitHub client (Ocktokit): https://github.com/actions/toolkit/tree/master/packages/github#usage
+  const github = GitHub.getOctokit(process.env.GITHUB_TOKEN);
+
+  const { data: branches } = await github.repos.listBranches({ owner: currentOwner, repo: currentRepo });
+
+  // check to make sure the API returned the correct data type
+  if (!Array.isArray(branches)) {
+    throw new Error('Expected Array of branches from GitHub API');
+  }
+
+  const sorted = branches
+    .map((item) => item.name) // we only care about branch names
+    .filter((item) => item.startsWith(config.pattern)) // ignore non-release branches for sort
+    .map((item) => item.split(config.pattern)[1]) // version compare needs the prefix removed
+    .filter(semver.valid) // remove any branches that will not be valid for sorting upon
+    .sort(semver.compare) // sorts lowest to highest version
+    .map((item) => config.pattern + item); // put the release branch pattern back
+
+  // put prod branch name at front of branch list
+  sorted.unshift(config.prod);
+
+  // put dev branch at back of branch list, if it was set
+  if (`${config.dev}` !== '') {
+    sorted.push(config.dev);
+  }
+
+  return sorted;
+}
+
+/**
+ *
+ * @param config {prod: string, dev: string, pattern: string, strategy}
+ * @returns {Promise<{action: string, source: string, target: string}>}
+ */
+async function findDownStreamBranch(config) {
+  const branchHierarchy = await getBranchHierarchy(config);
+  const { ref: currentBranchRef } = GitHub.context.repo;
+  const srcBranch = currentBranchRef.split('refs/heads/')[1];
+
+  console.log(branchHierarchy, srcBranch, config);
+  const nextBranchIndex = branchHierarchy.indexOf(srcBranch) + 1;
+
+  if (nextBranchIndex === 0 || nextBranchIndex === branchHierarchy.length) {
+    return {
+      source: srcBranch,
+      target: '',
+      action: MERGE_ACTIONS.NONE
+    };
+  }
+  else {
+    return {
+      source: srcBranch,
+      target: branchHierarchy[nextBranchIndex],
+      action: MERGE_ACTIONS.MERGE
+    };
+  }
+}
 
 async function run() {
   try {
-    // Get authenticated GitHub client (Ocktokit): https://github.com/actions/toolkit/tree/master/packages/github#usage
-    const github = GitHub.getOctokit(process.env.GITHUB_TOKEN);
+    const config = {
+      prod: core.getInput('production-branch', { required: false }) || 'master',
+      dev: core.getInput('development-branch', { required: false }) || '',
+      pattern: core.getInput('merge-pattern', { required: false }) || 'release/',
+      strategy: core.getInput('merge-strategy', { required: false }) || 'merge-no-ff'
+    };
 
-    // Get owner and repo from context of payload that triggered the action
-    const { owner: currentOwner, repo: currentRepo } = GitHub.context.repo;
+    const mergeSpec = await findDownStreamBranch(config);
 
-    const prodBranch = core.getInput('production-branch', { required: false }) || 'master';
-    const devBranch = core.getInput('development-branch', { required: false }) || 'develop';
-    const mergePattern = core.getInput('merge-pattern', { required: false }) || 'release/';
-    const mergeStrategy = core.getInput('merge-strategy', { required: false }) || 'merge-no-ff';
+    console.log(mergeSpec);
 
-    const owner = core.getInput('owner', { required: false }) || currentOwner;
-    const repo = core.getInput('repo', { required: false }) || currentRepo;
-
-    const { data: branchList } = await github.repos.listBranches({ owner, repo });
-
-    console.log(prodBranch, devBranch, mergePattern, mergeStrategy, owner, repo, branchList);
-
-    core.setOutput('details', "I haven't done anything yet");
+    core.setOutput('details', '');
   } catch (error) {
     core.setFailed(error.message);
   }
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,60 +1,154 @@
+const core = require('@actions/core');
+const GitHub = require('@actions/github');
+const run = require('../src/automerge.js');
+
 jest.mock('@actions/core');
 jest.mock('@actions/github');
 
-// const core = require('@actions/core');
-// const { github, context } = require('@actions/github');
-// const run = require('../src/automerge.js');
+const branchSample = {
+  data: [
+    {
+      name: 'develop',
+      protected: false
+    },
+    {
+      name: 'master',
+      protected: false
+    },
+    {
+      name: 'release/1.2.1-beta1',
+      protected: false
+    },
+    {
+      name: 'release/1.0.0-RC1',
+      protected: false
+    },
+    {
+      name: 'release/1.0.0',
+      protected: false
+    },
+    {
+      name: 'release/1.1.0',
+      protected: false
+    },
+    {
+      name: 'release/1.2.1',
+      protected: false
+    },
+    {
+      name: 'release/1.2',
+      protected: false
+    },
+    {
+      name: 'release/1.11.0',
+      protected: false
+    },
+    {
+      name: 'release/old_0.0.1',
+      protected: false
+    },
+    {
+      name: 'feature/abc',
+      protected: false
+    }
+  ]
+};
 
-describe('List Branches', () => {
+/* eslint-disable no-undef */
+describe('Valid Branch Workflows', () => {
   let listBranches;
 
   beforeEach(() => {
-    listBranches = '';
+    listBranches = jest.fn().mockReturnValueOnce(branchSample);
+
+    GitHub.getOctokit = jest.fn().mockReturnValueOnce({
+      repos: {
+        listBranches
+      }
+    });
+
+    core.getInput = jest
+      .fn()
+      .mockReturnValueOnce('master')
+      .mockReturnValueOnce('develop')
+      .mockReturnValueOnce('release/')
+      .mockReturnValueOnce('merge-no-ff')
+      .mockReturnValueOnce(null)
+      .mockReturnValueOnce(null);
+
+    GitHub.context.repo = {
+      owner: 'owner',
+      repo: 'repo',
+      ref: ''
+    };
   });
 
-  test('I do things', async () => {
-    expect(listBranches).toMatch('');
+  test('Master Branch Triggered', async () => {
+    GitHub.context.repo.ref = 'refs/heads/master';
+
+    await run();
+
+    expect(listBranches).toHaveBeenCalledWith({
+      owner: 'owner',
+      repo: 'repo'
+    });
+  });
+
+  test('Develop Branch Triggered', async () => {
+    GitHub.context.repo.ref = 'refs/heads/develop';
+
+    await run();
+
+    expect(listBranches).toHaveBeenCalledWith({
+      owner: 'owner',
+      repo: 'repo'
+    });
+  });
+
+  test('middle Branch Triggered', async () => {
+    GitHub.context.repo.ref = 'refs/heads/release/1.2.1-beta1';
+
+    await run();
+
+    expect(listBranches).toHaveBeenCalledWith({
+      owner: 'owner',
+      repo: 'repo'
+    });
   });
 });
 
-/* eslint-disable no-undef */
-// describe('List Branches', () => {
-//   let listBranches;
-//
-//   beforeEach(() => {
-//     listBranches = jest.fn().mockReturnValueOnce({
-//       data: {
-//         details: 'details'
-//       }
-//     });
-//
-//     context.repo = {
-//       owner: 'owner',
-//       repo: 'repo'
-//     };
-//
-//     const github = {
-//       repos: { listBranches }
-//     };
-//
-//     GitHub.mockImplementation(() => github);
-//   });
-//
-//   test('AutoMerge endpoint is called', async () => {
-//     core.getInput = jest
-//       .fn()
-//       .mockReturnValueOnce('master')
-//       .mockReturnValueOnce('develop')
-//       .mockReturnValueOnce('release/')
-//       .mockReturnValueOnce('merge-no-ff')
-//       .mockReturnValueOnce(null)
-//       .mockReturnValueOnce(null);
-//
-//     await run();
-//
-//     expect(listBranches).toHaveBeenCalledWith({
-//       owner: 'owner',
-//       repo: 'repo'
-//     });
-//   });
-// });
+describe('Invalid Ref Received', () => {
+  let listBranches;
+
+  beforeEach(() => {
+    listBranches = jest.fn().mockReturnValueOnce(branchSample);
+
+    GitHub.context.repo = {
+      owner: 'owner',
+      repo: 'repo',
+      ref: 'refs/tags/release/2.0.0'
+    };
+
+    GitHub.getOctokit = jest.fn().mockReturnValueOnce({
+      repos: {
+        listBranches
+      }
+    });
+  });
+
+  test('I do things', async () => {
+    core.getInput = jest
+      .fn()
+      .mockReturnValueOnce('master')
+      .mockReturnValueOnce('develop')
+      .mockReturnValueOnce('release/')
+      .mockReturnValueOnce('merge-no-ff')
+      .mockReturnValueOnce(null)
+      .mockReturnValueOnce(null);
+
+    await run();
+
+    expect(core.setFailed).toHaveBeenCalled();
+    expect(listBranches).toHaveBeenCalledTimes(0);
+  });
+});
